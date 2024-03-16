3c435db07c55b21f7976f7aafebb1bf4146e685f
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-16 04:15:32 +0300

    Added images styling

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -5,9 +5,32 @@ import { MEALS } from '../data/dummy-data';
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration
+    };
+    return (
+      <MealItem {...mealItemProps} />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId} </Text>
+            <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
