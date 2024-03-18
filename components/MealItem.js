9e9aae5a20464745a36da444e9361afea8f39c54
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-19 02:10:05 +0300

    Update

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,4 +1,3 @@
-import { useNavigation } from "@react-navigation/native";
 import {
   View,
   Pressable,
@@ -6,7 +5,9 @@ import {
   Image,
   StyleSheet,
   Platform,
-} from "react-native";
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 import MealDetails from './MealDetails';
 
 function MealItem({
@@ -18,11 +19,19 @@ function MealItem({
   affordability,
 }) {
   const navigation = useNavigation();
+
+  function selectMealItemHandler() {
+    navigation.navigate('MealDetail', {
+      mealId: id,
+    });
+  }
+
   return (
     <View style={styles.mealItem}>
       <Pressable
-        android_ripple={{ color: "#ccc" }}
+        android_ripple={{ color: '#ccc' }}
         style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        onPress={selectMealItemHandler}
       >
         <View style={styles.innerContainer}>
           <View>
@@ -46,10 +55,10 @@ const styles = StyleSheet.create({
   mealItem: {
     margin: 16,
     borderRadius: 8,
-    overflow: Platform.OS === "android" ? "hidden" : "visible",
-    backgroundColor: "white",
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    backgroundColor: 'white',
     elevation: 4,
-    shadowColor: "black",
+    shadowColor: 'black',
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
@@ -59,15 +68,15 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     borderRadius: 8,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   image: {
-    width: "100%",
+    width: '100%',
     height: 200,
   },
   title: {
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
     fontSize: 18,
     margin: 8,
   },
