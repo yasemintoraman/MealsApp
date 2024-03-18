610610f9dc9c96ce0a12c3284defe0bb49561eb0
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-18 23:29:45 +0300

    Meal Details Screen completed

--- App.js
+++ App.js
@@ -38,6 +38,7 @@ export default function App() {
             //   };
             // }}
           />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
