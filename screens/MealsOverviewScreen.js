2aa5ac1660c8c301b8d95eaa4d243e5ca777c2ac
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-18 22:54:10 +0300

    navigation options dynamically(useLayoutEffect)

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useLayoutEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+import { MEALS, CATEGORIES} from "../data/dummy-data";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
@@ -17,16 +29,14 @@ function MealsOverviewScreen({ route }) {
       imageUrl: item.imageUrl,
       affordability: item.affordability,
       complexity: item.complexity,
-      duration: item.duration
+      duration: item.duration,
     };
-    return (
-      <MealItem {...mealItemProps} />
-    );
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
     <View style={styles.container}>
-            <FlatList
+      <FlatList
         data={displayedMeals}
         keyExtractor={(item) => item.id}
         renderItem={renderMealItem}
